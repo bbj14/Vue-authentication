da347168d23dc31e5fe5d4fe903c12ec259319b2
bbj14 <kazuya1425m@gmail.com>
2020-07-19 17:55:48 +0900

    Lesson4 user login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="home">
     <h1>Welcome to the App!</h1>
+    <div v-if="!loggedIn">
+      <router-link to="/login">
+        Login
+      </router-link>
+      or
+      <router-link to="/register">
+        Register
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { authComputed } from '../vuex/helpers.js'
+export default {
+  computed: {
+    ...authComputed
+  }
+}
 </script>
